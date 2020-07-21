a1f894777884c761e36631fc2c4731678196bc9a
ZhJiaYi <769052153@qq.com>
2020-07-21 21:03:47 +0800

    修改拦截器，请求方法

--- src/common/http.api.js
+++ src/common/http.api.js
@@ -10,18 +10,32 @@ const install = (Vue, vm) => {
         })
     }
 
-    //获取微信登录链接
-    let getLoginUrl = (redirect_url) => vm.$u.get('/api/Auth/getOfficialAccountLoginUrl', {
-        redirect_url: redirect_url
-    })
+    function get(url, params) {
+        return new Promise((resolve, reject) => {
+            vm.$u.get(url, params).then(res => {
+                resolve(res)
+            }).catch(err => {
+                reject(err)
+            })
+        })
+    }
+
+    function post(url, params) {
+        return new Promise((resolve, reject) => {
+            vm.$u.post(url, params).then(res => {
+                resolve(res)
+            }).catch(err => {
+                reject(err)
+            })
+        })
+    }
     //利用code获取token
-    let getToken = (code) => vm.$u.post('/api/Auth/officialAccountLogin', {
+    let getToken = (code) => post('/api/Auth/officialAccountLogin', {
         code: code
     })
 
     // 将各个定义的接口名称，统一放进对象挂载到vm.$u.api(因为vm就是this，也即this.$u.api)下
     vm.$u.api = {
-        getLoginUrl,
         getToken
     };
 }
